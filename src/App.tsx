d50b2549ee934ecf87d5cd29ed46ed82b4745be9
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2025-09-10 19:16:54 +0300

    add react

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Cart1 from './components/Cart-1/Cart1';
+
+function App() {
+
+  return (
+    <>
+    <Cart1 />
+   
+    </>
+  )
+}
+
+export default App
